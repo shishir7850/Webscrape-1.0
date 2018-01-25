c72b132d3ceb3af38af39eb529c842e447052953
shishir7850 <slakkadi@purdue.edu>
2018-01-24 23:57:21 -0500

    First Commit

---
+++ scrape.py
@@ -0,0 +1,16 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+table = soup.find('tbody', attrs={'class': 'stripe'})
+
+for row in table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text)
+    print list_of_cells
